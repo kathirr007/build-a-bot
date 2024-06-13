670dc2b72f57f55f0ca229fcf6df38c60332b008
K <kathiravan.k@capgemini.com>
2024-06-13 17:59:34 +0530

    added homepage and robotbuilder pages

---
+++ src/build/RobotBuilder.vue
@@ -0,0 +1,207 @@
+<template>
+<div>
+  <div class="top-row">
+    <div class="top part">
+      <img :src="availableParts.heads[selectedHeadIndex].imageUrl" alt="head" />
+      <button @click="selectPreviousPart('heads', 'selectedHeadIndex')" class="prev-selector">
+        &#9668;
+      </button>
+      <button @click="selectNextPart('heads', 'selectedHeadIndex')" class="next-selector">
+        &#9658;
+      </button>
+    </div>
+  </div>
+  <div class="middle-row">
+    <div class="left part">
+      <img :src="availableParts.arms[selectedLeftArmIndex].imageUrl" alt="left arm" />
+      <button  @click="selectPreviousPart('arms', 'selectedLeftArmIndex')" class="prev-selector">
+        &#9650;
+      </button>
+      <button
+      @click="selectNextPart('arms', 'selectedLeftArmIndex')"
+       class="next-selector">&#9660;</button>
+    </div>
+    <div class="center part">
+      <img :src="availableParts.torsos[selectedTorsoIndex].imageUrl" alt="torso" />
+      <button
+      @click="selectNextPart('torsos', 'selectedTorsoIndex')"
+       class="prev-selector">&#9668;</button>
+      <button
+      @click="selectNextPart('torsos', 'selectedTorsoIndex')"
+       class="next-selector">&#9658;</button>
+    </div>
+    <div class="right part">
+      <img :src="availableParts.arms[selectedRightArmIndex].imageUrl" alt="right arm" />
+      <button
+      @click="selectNextPart('arms', 'selectedRightArmIndex')"
+       class="prev-selector">&#9650;</button>
+      <button
+      @click="selectNextPart('arms', 'selectedRightArmIndex')"
+       class="next-selector">&#9660;</button>
+    </div>
+  </div>
+  <div class="bottom-row">
+    <div class="bottom part">
+      <img :src="availableParts.bases[selectedBaseIndex].imageUrl" alt="base" />
+      <button
+      @click="selectNextPart('bases', 'selectedBaseIndex')"
+       class="prev-selector">&#9668;</button>
+      <button
+      @click="selectNextPart('bases', 'selectedBaseIndex')"
+       class="next-selector">&#9658;</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import parts from '../data/parts';
+
+function getNextValidIndex(index, length) {
+  const incrementedIndex = index + 1;
+  return incrementedIndex > length - 1 ? 0 : incrementedIndex;
+}
+
+function getPreviousValidIndex(index, length) {
+  const decrementedIndex = index - 1;
+  return decrementedIndex < 0 ? length - 1 : decrementedIndex;
+}
+
+export default {
+  name: 'RobotBuilder',
+  data() {
+    return {
+      availableParts: parts,
+      selectedHeadIndex: 0,
+      selectedLeftArmIndex: 0,
+      selectedTorsoIndex: 0,
+      selectedRightArmIndex: 0,
+      selectedBaseIndex: 0,
+    };
+  },
+  methods: {
+    selectNextPart(partName = 'heads', partIndex = 'selectedHeadIndex') {
+      this[partIndex] = getNextValidIndex(
+        this[partIndex],
+        this.availableParts[partName].length,
+      );
+    },
+    selectPreviousPart(partName = 'heads', partIndex = 'selectedHeadIndex') {
+      this[partIndex] = getPreviousValidIndex(
+        this[partIndex],
+        this.availableParts[partName].length,
+      );
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.part {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  border: 3px solid #aaa;
+}
+
+.part img {
+  width: 200px;
+}
+
+.top-row {
+  display: flex;
+  justify-content: space-around;
+}
+
+.middle-row {
+  display: flex;
+  justify-content: center;
+}
+
+.bottom-row {
+  display: flex;
+  justify-content: space-around;
+  border-top: none;
+}
+
+.top {
+  border-bottom: none;
+}
+
+.left {
+  border-right: none;
+}
+
+.right {
+  border-left: none;
+}
+
+.left img {
+  transform: rotate(-90deg);
+}
+
+.right img {
+  transform: rotate(90deg);
+}
+
+.bottom {
+  border-top: none;
+}
+
+.prev-selector {
+  position: absolute;
+  z-index: 1;
+  top: -3px;
+  left: -28px;
+  width: 25px;
+  height: 206px;
+}
+
+.next-selector {
+  position: absolute;
+  z-index: 1;
+  top: -3px;
+  right: -28px;
+  width: 25px;
+  height: 206px;
+}
+
+.center .prev-selector,
+.center .next-selector {
+  opacity: 0.8;
+}
+
+.left .prev-selector {
+  top: -28px;
+  left: -3px;
+  width: 179px;
+  height: 25px;
+}
+
+.left .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: -3px;
+  width: 179px;
+  height: 25px;
+}
+
+.right .prev-selector {
+  top: -28px;
+  left: 24px;
+  width: 179px;
+  height: 25px;
+}
+
+.right .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: 24px;
+  width: 179px;
+  height: 25px;
+}
+
+.right .next-selector {
+  right: -3px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    devtool: 'source-map',
+  },
 });
